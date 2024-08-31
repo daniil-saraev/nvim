7bd9d413ea9106c46a7db852e3f5e87d2651f337
Saraev Daniil <Daniil.Saraev@infotecs.ru>
2024-08-31 10:59:34 +0300

    initial

---
+++ lua/plugins/user.lua
@@ -0,0 +1,81 @@
+return {
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  {
+    "fedepujol/move.nvim",
+    lazy = false,
+    config = function()
+      require("move").setup {
+        line = {
+          enable = true, -- Enables line movement
+          indent = true, -- Toggles indentation
+        },
+        block = {
+          enable = true, -- Enables block movement
+          indent = true, -- Toggles indentation
+        },
+        word = {
+          enable = true, -- Enables word movement
+        },
+        char = {
+          enable = true, -- Enables char movement
+        },
+      }
+    end,
+  },
+  {
+    "nvim-telescope/telescope-file-browser.nvim",
+    lazy = false,
+    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+    config = function()
+      local fb_actions = require("telescope").extensions.file_browser.actions
+      require("telescope").setup {
+        extensions = {
+          file_browser = {
+            display_stat = { date = true, size = false, mode = false },
+            theme = "ivy",
+            -- disables netrw and use telescope-file-browser in its place
+            hijack_netrw = true,
+            mappings = {
+              ["i"] = {
+                -- your custom insert mode mappings
+              },
+              ["n"] = {
+                ["<A-a>"] = fb_actions.create,
+              },
+            },
+          },
+        },
+      }
+      -- To get telescope-file-browser loaded and working with telescope,
+      -- you need to call load_extension, somewhere after setup function:
+      require("telescope").load_extension "file_browser"
+    end,
+  },
+  {
+    "pappasam/papercolor-theme-slim",
+    lazy = false,
+    config = function()
+	vim.cmd('colorscheme PaperColorSlim')
+	end
+  },
+  {
+    "AstroNvim/astrocore",
+    ---@param opts AstroCoreOpts
+    opts = {
+      options = {
+        opt = {
+          showtabline = 0,
+        },
+      },
+    },
+  },
+  {
+    "AstroNvim/astrolsp",
+    ---@type AstroLSPOpts
+    opts = {
+      features = {
+        signature_help = true, -- enable automatic signature help popup globally on startup
+      },
+    },
+  },
+}
